1d5ee7916aff67824bcb6b5f5927a57d715cc269
gucci1909 <umangarora0134@gmail.com>
2022-12-14 00:42:22 +0530

    my-first-java-project

---
+++ src/checking.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class checking {
+    public static void main(String[] args) {
+        Scanner checking = new Scanner(System.in);
+        boolean checking_1 = checking.hasNextInt();
+        System.out.println(checking_1);
+    }
+}
---
+++ src/hello.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[] args) {
+        Scanner name = new Scanner(System.in);
+        String name_1 = name.next();
+        System.out.println("Hello "+name_1+" , how are you?");
+    }
+}
